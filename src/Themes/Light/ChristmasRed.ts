3e1fba2ad418d4709aa15a0fd3fc2deeec5b8d38
Wiwok <84347864+Wiwok@users.noreply.github.com>
2022-12-10 15:22:56 +0100

    Fixed christmas theme border color

--- src/Themes/Light/ChristmasRed.ts
+++ src/Themes/Light/ChristmasRed.ts
@@ -1,6 +1,6 @@
 const ChristmasRed = {
 	'backgroundImage': 'url("https://menuvox.fr/Assets/Christmas/2.png"), linear-gradient(#E6361A 50%, #CD2B17 50%)',
-	'border': '0.2vmax solid #56D6AB'
+	'border': '0.2vmax solid #E6361A'
 }
 
 export default ChristmasRed;
